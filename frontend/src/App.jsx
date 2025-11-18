d44fb2727fd1fb552af43c6a7bbcfe5054bc26c8
Rron Dedushi <rrondedushi@gmail.com>
2025-11-18 17:07:03 -0500

    Add Home route and update routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Categories from "./pages/Categories.jsx";
 import StepGenerator from "./pages/StepGenerator.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Categories />} />
-      <Route path="/generate/:id" element={<StepGenerator />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/category/:categoryId" element={<StepGenerator />} />
     </Routes>
   );
 }

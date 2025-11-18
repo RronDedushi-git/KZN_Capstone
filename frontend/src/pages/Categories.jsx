0f20f40557a5b88c5b0cf769abca8f4359be6ece
Rron Dedushi <rrondedushi@gmail.com>
2025-11-18 15:07:20 -0500

    Render categories list in Categories page

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -3,10 +3,24 @@ import API from "../hooks/useApi";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
-}
 
-useEffect(() => {
-  API.get("/categories")
-    .then((res) => setCategories(res.data))
-    .catch((err) => console.log(err));
-}, []);
+  useEffect(() => {
+    API.get("/categories")
+      .then((res) => setCategories(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Choose a Category</h1>
+
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat._id}>
+            {cat.icon} — {cat.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
